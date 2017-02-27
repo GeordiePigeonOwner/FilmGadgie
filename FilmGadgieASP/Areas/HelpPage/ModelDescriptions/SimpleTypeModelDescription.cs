bc2964b928fd37da8c4ed1e728153b382ec53391
JBeresford-home <j.beresford@uk.cdw.com>
2017-02-27 16:57:39 +0000

    Add project files.

---
+++ FilmGadgieASP/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FilmGadgieASP.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FilmGadgieASP/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FilmGadgieASP.Startup))]
+
+namespace FilmGadgieASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
